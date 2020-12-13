af80791acb985ba6269ca838254c8880cd7ed97e
lain-Dev <55925589+lain-Dev@users.noreply.github.com>
2020-12-13 20:19:32 +0400

    formulaire en php

--- Openclassroom-PHP/06-formulaire/formulaire.php
+++ Openclassroom-PHP/06-formulaire/formulaire.php
@@ -43,7 +43,30 @@
                 Formulaire d'envoi de fichier :<br />
                 <input type="file" name="monfichier" /><br />
                 <input type="submit" value="Envoyer le fichier" />
-        </p>
+		</p>
+		
+		<?php
+			// Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+			if (isset($_FILES['monfichier']) AND $_FILES['monfichier']['error'] == 0)
+			{
+					// Testons si le fichier n'est pas trop gros
+					if ($_FILES['monfichier']['size'] <= 1000000)
+					{
+							// Testons si l'extension est autorisée
+							$infosfichier = pathinfo($_FILES['monfichier']['name']);
+							$extension_upload = $infosfichier['extension'];
+							$extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+							if (in_array($extension_upload, $extensions_autorisees))
+							{
+									// On peut valider le fichier et le stocker définitivement
+									move_uploaded_file($_FILES['monfichier']['tmp_name'], 'uploads/' . basename($_FILES['monfichier']['name']));
+									echo "L'envoi a bien été effectué !";
+							}
+					}
+			}
+			/*Lorsque vous mettrez le script sur Internet à l'aide d'un logiciel FTP, vérifiez que le dossier « Uploads » sur le serveur existe, et qu'il a les droits d'écriture. Pour ce faire, sous FileZilla par exemple, faites un clic droit sur le dossier et choisissez « Attributs du fichier ».
+			Cela vous permettra d'éditer les droits du dossier (on parle de CHMOD). Mettez les droits à 733, ainsi PHP pourra placer les fichiers uploadés dans ce dossier. */
+		?>
 
 		<p>
 			<input type="text" name="prenom" /> <input type="submit" value="Valider" />
